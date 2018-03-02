b616c95e77803994e89012d693660e05a8df1a85
Savas <tomas1savickas@gmail.com>
2018-03-02 22:20:10 +0000

    Update visual sound making code, add killing sound

--- Silentium/Assets/Scripts/StepSoundMaker.cs
+++ Silentium/Assets/Scripts/StepSoundMaker.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StepSoundMaker : MonoBehaviour {
+public class StepSoundMaker : SoundMaker {
 	public float SteppingFrequency = 1f;
 
 	private float DEFAULT_STEPPING_FREQUENCY;
 
-	private ParticleSystem _stepParticles;
 	private Transform _transform;
 	private Vector2 _previousPosition;
 
 	private void Start() {
+		base.Start ();
+
 		DEFAULT_STEPPING_FREQUENCY = SteppingFrequency;
-		_stepParticles = GetComponentInChildren<ParticleSystem>();
 		_transform = GetComponent<Transform> ();
 	
 		SavePosition();
@@ -21,7 +21,7 @@ public class StepSoundMaker : MonoBehaviour {
 
 	private void Update() {
 		if (ShouldMakeAStep()) {
-			MakeSteppingSound ();
+			MakeSound();
 			SavePosition ();
 		}
 	}
@@ -32,10 +32,6 @@ public class StepSoundMaker : MonoBehaviour {
 
 	private bool ShouldMakeAStep() {
 		return Vector2.Distance (_transform.position, _previousPosition) >= SteppingFrequency;
-	}
-
-	private void MakeSteppingSound() {
-		_stepParticles.Play ();
 	}
 
 	public void ResetSteppingFrequency() {
