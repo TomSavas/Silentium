7d409188fe3752738a71c25e4699a6556cab0fc9
Unknown <pijuskri@gmail.com>
2018-03-03 05:27:44 +0200

    addes Police civ ai, movement

---
+++ Silentium/Assets/Scripts/PathFinding/CivilianAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianAI : MonoBehaviour {
+
+    public List<Transform> waypoints;
+    public int currentWaypoint = 0;
+    float speed = 3f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.1f) currentWaypoint++;
+        if (currentWaypoint >= waypoints.Capacity) currentWaypoint = 0;
+
+        var dir = waypoints[currentWaypoint].position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Silentium/Assets/Scripts/PathFinding/PoliceAI.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceAI : MonoBehaviour
+{
+
+    #region variables
+    public List<Transform> waypoints;
+    public int currentWaypoint = 0;
+    float speed = 3f;
+    public int mode = 0;
+    public GameObject soundOriginGameObject;
+    public Transform soundOrigin;
+    #endregion
+
+    void Start()
+    {
+        soundOriginGameObject = new GameObject();
+        soundOrigin = soundOriginGameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        #region Mode control
+        if (Input.GetMouseButtonDown(0))
+        {
+            soundOrigin.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            soundOrigin.position = new Vector3(soundOrigin.position.x, soundOrigin.position.y, 0);
+            mode = 1;
+            changeAIMode();
+        }
+        /*if (mode == 1)
+        {
+            if (Vector3.Distance(transform.position, soundOrigin.position) < 0.3f)
+            {
+                mode = 2;
+                changeAIMode();
+            }
+        }
+        if (mode == 2)
+        {
+            if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.3f)
+            {
+                mode = 0;
+            }
+        }*/
+
+        #endregion
+        #region Waypoints (mode 0)
+        if (mode == 0)
+        {
+            if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.1f) currentWaypoint++;
+            if (currentWaypoint >= waypoints.Capacity) currentWaypoint = 0;
+
+            var dir = waypoints[currentWaypoint].position - transform.position;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        #endregion
+    }
+    public void changeAIMode()
+    {
+        #region Alert (mode 1)
+        if (mode == 1)
+        {
+            gameObject.GetComponent<Unit>().target = soundOrigin;
+            gameObject.GetComponent<Unit>().PathFindToTarget();
+        }
+        #endregion
+        #region Return to patrolling(mode 2)
+        if (mode == 2)
+        {
+            gameObject.GetComponent<Unit>().target = waypoints[currentWaypoint];
+            gameObject.GetComponent<Unit>().PathFindToTarget();
+        }
+        #endregion
+    }
+    public void changeMode()
+    {
+        if (mode == 1)
+        {
+            mode = 2;
+        }
+        else if (mode == 2)
+        {
+            mode = 0;
+        }
+        changeAIMode();
+    }
+}
