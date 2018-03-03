7cae72ed2ee44fefcc6ebf76502bea6e1d92bb30
Unknown <pijuskri@gmail.com>
2018-03-03 22:00:25 +0200

    Shooting wip, waiting for rotation

---
+++ Silentium/Assets/Scripts/AI/PoliceShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceShoot : MonoBehaviour {
+
+    bool canSeePlayer=false;
+    float timeToShoot = 0;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        int layer1 = 8;
+        int layer2 = 11;
+        int layermask1 = 1 << layer1;
+        int layermask2 = 1 << layer2;
+        int finalmask = layermask1 | layermask2;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 10, finalmask);
+        if (hit == true)
+        {
+            print("i see you");
+            if(hit.collider.gameObject.tag=="Player") canSeePlayer = true;
+        }
+        else canSeePlayer = false;
+
+        if (canSeePlayer)
+        {
+            timeToShoot += Time.deltaTime;
+        }
+        else timeToShoot = 0;
+
+        if (timeToShoot>1)
+        {
+            print("DEAD");
+        }
+	}
+}
