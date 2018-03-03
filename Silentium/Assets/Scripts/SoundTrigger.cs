807e23bd37de8f65c6f0c8a4f5c725e020b1490c
Savas <tomas1savickas@gmail.com>
2018-03-03 08:53:45 +0000

    Add panic trigger by sound particles

---
+++ Silentium/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour {
+	public CivilianAI Civilian;
+
+	private void OnParticleCollision(GameObject particle) {
+		if (particle.CompareTag ("Sound")) {
+			Civilian.TriggerPanic ();
+		}
+	}
+}
