8664a5609476c98e25f43044795f3a1701aabc4d
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2018-03-04 04:26:54 +0200

    add MenuSound

---
+++ Silentium/Assets/Scripts/Sound/BackgroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	public AudioSource intro;
+	public AudioSource loop;
+
+	// Use this for initialization
+	void Start () {
+		intro.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!intro.isPlaying && !loop.isPlaying) {
+			loop.Play ();
+		}
+	}
+}
