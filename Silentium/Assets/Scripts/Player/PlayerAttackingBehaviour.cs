b23306f40eae6a0e4678c48d7d1c9df745b1946d
Savas <tomas1savickas@gmail.com>
2018-03-04 02:01:40 +0000

    Add killing (sound waves don't work yet)

--- Silentium/Assets/Scripts/Player/PlayerAttackingBehaviour.cs
+++ Silentium/Assets/Scripts/Player/PlayerAttackingBehaviour.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerAttackingBehaviour : MonoBehaviour {
 	public Animator slash;
 
+	private void Update() {
+		if (slash.IsInTransition (0)) {
+			Destroy (slash.gameObject.GetComponent<PolygonCollider2D> ());
+			var polyCollider = slash.gameObject.AddComponent<PolygonCollider2D> ();
+			polyCollider.isTrigger = true;
+		}
+	}
+
 	private void FixedUpdate() {
 		if (Input.GetKeyDown (KeyCode.Return)) {
 			slash.speed = 2.5f;
 			slash.SetBool ("Slash", true);
 		} else {
 			slash.SetBool ("Slash", false);
+			Destroy (slash.gameObject.GetComponent<PolygonCollider2D> ());
 		}
 	}
 }
