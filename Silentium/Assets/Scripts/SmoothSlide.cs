009d93eeba7e64e7d9c2016566386cafe7edcc39
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2018-03-04 02:58:40 +0200

    add TextBox to GUI

---
+++ Silentium/Assets/Scripts/SmoothSlide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothSlide : MonoBehaviour {
+
+	void Start () {
+		StartCoroutine (Transit () );
+	}
+
+	public GameObject centre;
+
+	void Go() {
+		transform.position = Vector3.Lerp (transform.position, centre.transform.position, 0.1f);
+	}
+
+	private IEnumerator Transit(){
+		int n = 0;
+		while (Vector3.Distance (transform.position, centre.transform.position) >= 0.01) {
+			Go ();
+			yield return null;
+		}
+	}
+}
